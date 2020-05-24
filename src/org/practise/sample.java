f5a2057f4675010a5d653907d7bf69277f213e49
gopinathsharan <gopinath021990@gmail.com>
2020-05-24 16:47:31 +0530

    selinium-project-5pmbatch

---
+++ src/org/practise/sample.java
@@ -0,0 +1,18 @@
+package org.practise;
+
+import javax.swing.border.EmptyBorder;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		public void company() {
+			System.out.println("empid");
+			
+		}
+		
+
+	}
+
+}
